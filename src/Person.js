58cf911e7303b80bb1b233989c5542585726be21
rich5k <richardanatsui@gmail.com>
2020-09-25 02:23:33 +0100

    created person component

---
+++ src/Person.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Person() {
+    return (
+        <div >
+            <h1>Max</h1>
+            <p>Your Age: 28</p>
+        </div>
+    )
+}
+
+export default Person
